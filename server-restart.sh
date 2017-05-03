ba34dc130e701f9ba5e0c00556345a73350d7144
zhgqthomas <zhgqthomas@gmail.com>
2017-05-03 14:50:34 +0800

    Add client script & config file

--- server-restart.sh
+++ server-restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Stopping Kcptun..."
-bash ~/kcptun/kcpsh/stop.sh
-bash ~/kcptun/kcpsh/start.sh
+bash ~/kcptun/kcpsh/server-stop.sh
+bash ~/kcptun/kcpsh/server-start.sh
 echo "Kcptun started."
